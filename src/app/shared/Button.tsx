5695b47d1eefe2e130ed71502c892b0ac19ce9f7
priyed <stanleyduye@gmail.com>
2026-03-19 09:46:44 +0100

    clean up

--- src/app/shared/Button.tsx
+++ src/app/shared/Button.tsx
@@ -21,7 +21,7 @@ export const MessageButton = ({ action, title }: ButtonProps) => {
 export const ResumeButton = () => {
   return (
     <Link
-      href="https://drive.google.com/file/d/1-eaEbE-PkrXyPoMsoCyrQbJxfYQaqOK0/view?usp=sharing"
+      href="https://drive.google.com/file/d/1EPzR-aIboMZO6PQke5b7k5CPLQx9h0He/view?usp=drive_link"
       target="_blank"
       className="p-[0.875rem] rounded-lg bg-none border-2 border-black text-xs lg:text-[1rem] font-semibold hover:bg-black hover:text-white transition-colors duration-300"
     >
